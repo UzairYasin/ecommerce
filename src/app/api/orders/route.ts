81ddda35075a24a652d96f1af4859167a16a5a05
Uzair <muzairbinyasin@gmail.com>
2024-09-23 12:38:49 +0500

    ship order updated

--- src/app/api/orders/route.ts
+++ src/app/api/orders/route.ts
@@ -1,7 +1,7 @@
 import { db, orderItems, orders } from "@/lib/schema";
 import { desc, eq } from "drizzle-orm";
 import { NextResponse } from "next/server";
-  
+
 export async function GET() {
 
     const Orders = await db.select().from(orders).orderBy(desc(orders.createdAt));
@@ -10,7 +10,7 @@ export async function GET() {
         Orders.map(async (order: any) => {
             const items = await db.select().from(orderItems).where(eq(orderItems.orderId, order.id))
 
-            return{
+            return {
                 ...order,
                 orderItems: items
             }
@@ -21,20 +21,21 @@ export async function GET() {
 
 }
 
-export async function PUT(req: Request){
+export async function PUT(req: Request) {
 
     const body = await req.json();
 
-    const res = await db.update(orders).set({
-        tracking_id: body.tracking_id,
-        shipping_status: body.shipping_status,
-    }).
-    where(eq(orders.id, body.id));
+    try {
+        const res = await db.update(orders).set({
+            tracking_id: body.tracking_id,
+            shipping_status: body.shipping_status,
+        }).where(eq(orders.id, body.id));
 
-    if(res){
-        return NextResponse.json("Shipped", {status: 200})
-    }else{
-        return NextResponse.json("error", {status: 400})
+        return NextResponse.json("Shipped", { status: 200 })
+    } catch (error) {
+        return NextResponse.json("error:"+error, {status:400})
     }
+
+
 }
 
